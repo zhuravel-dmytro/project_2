ba677df29525af44b57ac7820827772c1c07e618
Dimas <zhuravel16@gmail.com>
2019-08-22 00:50:49 +0300

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 
 var gulp = require('gulp'),
-less = require('gulp-less');
-
-gulp.task('mytask', function() {
-console.log('перевірка');
-});
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-rename');
 
 gulp.task('less', function(){
-return gulp.src('./app/less/**/*.less') 
-    .pipe(less())
+return gulp.src('./app/less/**/*.less')
+    .pipe(rename ({suffix: '.min'} ))
+    .pipe(less({
+        errorLogToConsole: true,
+        outputStyle: 'compressed'
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
     .pipe(gulp.dest('./app/css'))
 });
 
